e3e5d498b72c64050d438f1ed5bcbd80b0e73bad
leocuba88 <leocuba88@hotmail.com>
2021-06-22 00:15:15 +0000

    bottles milk

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,1 +1,17 @@
 // Your code here:
+const a = " bottles of milk on the wall, ";
+const b = " bottles of milk. Take one down and pass it around, ";
+const c = " bottles of milk on the wall.";
+const d = " no more bottles of milk on the wall.";
+function getSong() {
+    for (i = 99; i > 0; i--)
+    {
+ if (i != 1){
+ console.log(i + a + i + b + (i-1) +c);
+ }else if (i == 1){
+ console.log(i + a + i + b + d);
+ }
+            
+        }
+}
+getSong();
