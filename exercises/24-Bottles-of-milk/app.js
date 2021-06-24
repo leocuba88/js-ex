af1e61fe44772c004014ac1ed8e1bd05bd6c369a
leocuba88 <leocuba88@hotmail.com>
2021-06-24 22:26:21 +0000

    fix1

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -2,7 +2,7 @@
 const a = " bottles of milk on the wall, ";
 const b = " bottles of milk. Take one down and pass it around, ";
 const c = " bottles of milk on the wall.";
-const d = " no more bottles of milk on the wall.";
+const d = "no more bottles of milk on the wall.";
 function getSong() {
     for (i = 99; i > 0; i--)
     {
